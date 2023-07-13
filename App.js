a16560915858a5d239dddc159b3dd28820c7c6c3
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-07-13 16:12:21 +0200

    add LoginScreen

--- App.js
+++ App.js
@@ -2,24 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useFonts } from "expo-font";
 //* import * as Font from "expo-font";
 //* import { AppLoading } from "expo";
-import {
-  Platform,
-  ImageBackground,
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-  KeyboardAvoidingView,
-  Keyboard,
-  TouchableWithoutFeedback,
-  Dimensions,
-} from "react-native";
 
-const initialState = {
-  email: "",
-  password: "",
-};
+import LoginScreen from "./screens/auth/LoginScreen";
 
 //* const loadApplication = async () => {
 //*   await Font.loadAsync({
@@ -28,36 +12,12 @@ const initialState = {
 //* };
 
 export default function App() {
-  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
-  const [state, setstate] = useState(initialState);
   //* const [iasReady, setIasReady] = useState(false);
   const [fontsLoaded] = useFonts({
     "DMMono-Regular": require("./assets/Fonts/DMMono-Regular.ttf"),
     "DMMono-Medium": require("./assets/Fonts/DMMono-Medium.ttf"),
     "DMMono-Light": require("./assets/Fonts/DMMono-Light.ttf"),
   });
-  const [dimensions, setDimensions] = useState(
-    Dimensions.get("window").width - 20 * 2
-  );
-
-  useEffect(() => {
-    const onChange = () => {
-      const width = Dimensions.get("window").width - 20 * 2;
-      setDimensions(width);
-      console.log("width", width);
-    };
-    Dimensions.addEventListener("change", onChange);
-
-    return () => {
-      Dimensions.removeEventListener("change", onChange);
-    };
-  }, []);
-
-  const keyboardHide = () => {
-    setIsShowKeyboard(false);
-    Keyboard.dismiss();
-    setstate(initialState);
-  };
 
   //* if (!iasReady) {
   //*   return (
@@ -73,132 +33,8 @@ export default function App() {
   }
 
   return (
-    <TouchableWithoutFeedback onPress={keyboardHide}>
-      <View style={styles.container}>
-        <ImageBackground
-          style={styles.image}
-          source={require("./image/fentezi.jpg")}
-        >
-          <KeyboardAvoidingView
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-          >
-            <View
-              style={{
-                ...styles.form,
-                marginBottom: isShowKeyboard ? 20 : 100,
-                width: dimensions,
-              }}
-            >
-              <View style={styles.header}>
-                <Text style={styles.headerTitle}>Hello again</Text>
-                <Text style={styles.headerTitle}>Welcome back</Text>
-              </View>
-
-              <View>
-                <Text style={styles.inputTitle}>EMAIL ADDRES</Text>
-                <TextInput
-                  style={styles.input}
-                  onFocus={() => setIsShowKeyboard(true)}
-                  value={state.email}
-                  onChangeText={(value) =>
-                    setstate((prevState) => ({ ...prevState, email: value }))
-                  }
-                />
-              </View>
-              <View style={{ marginTop: 20 }}>
-                <Text style={styles.inputTitle}>PASSWORD</Text>
-                <TextInput
-                  style={styles.input}
-                  secureTextEntry={true}
-                  onFocus={() => setIsShowKeyboard(true)}
-                  value={state.password}
-                  onChangeText={(value) =>
-                    setstate((prevState) => ({
-                      ...prevState,
-                      password: value,
-                    }))
-                  }
-                />
-              </View>
-              <TouchableOpacity
-                style={styles.btn}
-                activeOpacity={0.8}
-                onPress={keyboardHide}
-              >
-                <Text style={styles.btnTitle}>SIGN IN</Text>
-              </TouchableOpacity>
-            </View>
-          </KeyboardAvoidingView>
-        </ImageBackground>
-      </View>
-    </TouchableWithoutFeedback>
+    <>
+      <LoginScreen />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    // justifyContent: "center",
-    // alignItems: "center",
-  },
-  text: {
-    color: "#ffffff",
-    fontSize: 30,
-  },
-  image: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-    // justifyContent: "flex-end",
-    alignItems: "center",
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#FFFFFF",
-    borderRadius: 6,
-    color: "#FFFFFF",
-    textAlign: "center",
-  },
-  form: {
-    // marginHorizontal: 40,
-  },
-  inputTitle: {
-    color: "#FFFFFF",
-    marginBottom: 10,
-    fontSize: 12,
-  },
-  btn: {
-    height: 40,
-    borderRadius: 6,
-    borderWidth: 1,
-    marginTop: 40,
-    justifyContent: "center",
-    alignItems: "center",
-    marginHorizontal: 100,
-
-    ...Platform.select({
-      ios: {
-        backgroundColor: "transparent",
-        borderColor: "#FFFFFF",
-      },
-      android: {
-        backgroundColor: "#1E90FF",
-        borderColor: "transparent",
-      },
-    }),
-  },
-  btnTitle: {
-    color: Platform.OS === "ios" ? "#1E90FF " : "#FFFFFF",
-    fontSize: 12,
-  },
-  header: {
-    alignItems: "center",
-    marginBottom: 100,
-  },
-  headerTitle: {
-    fontFamily: "DMMono-Regular",
-    fontSize: 30,
-    color: "#FFFFFF",
-  },
-});
