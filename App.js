999a4823885f9cc36cff2a6db9ccf81f4a7afdb7
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-07-17 19:44:43 +0200

    add icons  to menuBottom

--- App.js
+++ App.js
@@ -3,51 +3,14 @@ import { useFonts } from "expo-font";
 //* import * as Font from "expo-font";
 //* import { AppLoading } from "expo";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import LoginScreen from "./screens/auth/LoginScreen";
-import RegisterScreen from "./screens/auth/RegisterScreen";
-import PostsScreen from "./screens/mainScreen/PostsScreen";
-import CreateScreen from "./screens/mainScreen/CreateScreen";
-import ProfileScreen from "./screens/mainScreen/ProfileScreen";
-
-const AuthStack = createNativeStackNavigator();
-const MainTab = createBottomTabNavigator();
-
-// import { useRoute } from "./router";
+import { useRoute } from "./router";
 
 //* const loadApplication = async () => {
 //*   await Font.loadAsync({
 //*     "DMMono-Regular": require("./assets/Fonts/DMMono-Regular.ttf"),
 //*   });
 //* };
-
-const useRoute = (isAuth) => {
-  if (!isAuth) {
-    return (
-      <AuthStack.Navigator>
-        <AuthStack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <AuthStack.Screen
-          name="Register"
-          component={RegisterScreen}
-          options={{ headerShown: false }}
-        />
-      </AuthStack.Navigator>
-    );
-  }
-  return (
-    <MainTab.Navigator>
-      <MainTab.Screen name="Posts" component={PostsScreen} />
-      <MainTab.Screen name="Create" component={CreateScreen} />
-      <MainTab.Screen name="Profile" component={ProfileScreen} />
-    </MainTab.Navigator>
-  );
-};
 
 export default function App() {
   //* const [iasReady, setIasReady] = useState(false);
@@ -56,7 +19,7 @@ export default function App() {
     "DMMono-Medium": require("./assets/Fonts/DMMono-Medium.ttf"),
     "DMMono-Light": require("./assets/Fonts/DMMono-Light.ttf"),
   });
-  const routing = useRoute(null);
+  const routing = useRoute(true);
 
   //* if (!iasReady) {
   //*   return (
--- router.js
+++ router.js
@@ -3,14 +3,18 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+const AuthStack = createNativeStackNavigator();
+const MainTab = createBottomTabNavigator();
+
 import LoginScreen from "./screens/auth/LoginScreen";
 import RegisterScreen from "./screens/auth/RegisterScreen";
 import PostsScreen from "./screens/mainScreen/PostsScreen";
 import CreateScreen from "./screens/mainScreen/CreateScreen";
 import ProfileScreen from "./screens/mainScreen/ProfileScreen";
 
-const AuthStack = createNativeStackNavigator();
-const MainTab = createBottomTabNavigator();
+// icon
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 
 export const useRoute = (isAuth) => {
   if (!isAuth) {
@@ -30,10 +34,38 @@ export const useRoute = (isAuth) => {
     );
   }
   return (
-    <MainTab.Navigator>
-      <MainTab.Screen name="Posts" component={PostsScreen} />
-      <MainTab.Screen name="Create" component={CreateScreen} />
-      <MainTab.Screen name="Profile" component={ProfileScreen} />
+    <MainTab.Navigator tabBarOptions={{ showLabel: false }}>
+      <MainTab.Screen
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <MaterialCommunityIcons name="post" size={size} color={color} />
+          ),
+        }}
+        name="Posts"
+        component={PostsScreen}
+      />
+      <MainTab.Screen
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <Ionicons name="create" size={size} color={color} />
+          ),
+        }}
+        name="Create"
+        component={CreateScreen}
+      />
+      <MainTab.Screen
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <MaterialCommunityIcons
+              name="face-man-profile"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+        name="Profile"
+        component={ProfileScreen}
+      />
     </MainTab.Navigator>
   );
 };
